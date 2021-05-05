130a59c717f1fbd38dd2848740040c98a94eb19d
Nika Khachiashvili <o.sicknick@gmail.com>
2021-05-05 09:49:19 +0400

    updating form margin

--- client/src/themes/formDesign.ts
+++ client/src/themes/formDesign.ts
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(20),
+    marginTop: theme.spacing(10),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
